d48655c90b0984f188a4c8ecdd489f7506f8e974
liuranchao <kevinliu@qeeka.com>
2016-09-26 11:02:39 +0800

    创建CoreModule

---
+++ jiacore/src/main/java/com/jia/jiacore/IBaseApplication.java
@@ -0,0 +1,13 @@
+package com.jia.jiacore;
+
+import android.app.Application;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ * @date 16/9/26 上午10:57
+ */
+
+public class IBaseApplication extends Application {
+}
---
+++ jiacore/src/main/java/com/jia/jiacore/ui/IBaseActivity.java
@@ -0,0 +1,12 @@
+package com.jia.jiacore.ui;
+
+/**
+ * Description: TODO
+ *
+ * @author liuranchao
+ * @date 16/9/26 上午10:56
+ */
+
+public class IBaseActivity {
+
+}
